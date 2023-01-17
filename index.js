c97a871cb28d836dec83bf1216d7ea372600b8e5
kevinsoffera <kevinsoffera@gmail.com>
2023-01-16 22:27:41 -0500

    Added flex containers - need to fix alignment rows

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-let colors = []
+let colors = ["red", "orange", "yellow", "green", "blue", "indigo", "purple", "pink"]
+const list = document.getElementById("list")
 
-$(document).ready(function () {
 
-    $("button").click(function () {
-        $("h1").hide();
-    });
+let listItems = ""
+for (let i = 0; i < colors.length; i++) {
+    listItems += `
+        <div class="flex-item" style="background:${colors[i]}">
+            ${colors[i]}
+        </div>
 
-});
+    `
+    list.innerHTML = listItems
+
+}
+
+/* 
+Add functionality to sort colors
+*/
+
+// $(function () {
+
+//     $("button").click(function () {
+//         $("h1").hide();
+//     });
+
+// });
