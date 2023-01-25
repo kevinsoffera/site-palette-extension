1324b32cd5fcd7de5d02c36cda1eb0ba02acf030
kevinsoffera <kevinsoffera@gmail.com>
2023-01-24 22:13:36 -0500

    slight refactor, move towards getting CSS

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let colors = ["#8B0000", "red", "darkorange",
 
 const list = document.getElementById("list")
 const urlBtn = document.getElementById("url-btn")
+const urlCSS = document.getElementById('cssSheet')
 let url = document.getElementById("url")
 let urlString = ""
 
@@ -11,20 +12,46 @@ let urlString = ""
 urlBtn.addEventListener("click", function(){
     browser.tabs.query({active: true, currentWindow: true}, function(tabs){
         urlString = tabs[0].url
+        url.innerHTML = urlString
     })
+    renderColors()
+    GetCurrentDocument()
+    scrape()
+
 })
 
-
-let listItems = ""
-for (let i = 0; i < colors.length; i++) {
-    listItems += `
-        <div class="flex-item" style="background:${colors[i]}">
-            ${colors[i]}
-        </div>
-    `
-    
+function renderColors() {
+    let listItems = ""
+    for (let i = 0; i < colors.length; i++) {
+        listItems += `
+            <div class="flex-item" style="background:${colors[i]}">
+                ${colors[i]}
+            </div>
+        `
+        
+    }
+    list.innerHTML = listItems
 }
-list.innerHTML = listItems
+
+function scrape() {
+    $.each(document.styleSheets, function(sheetIndex, sheet) {
+        console.log("Looking at styleSheet[" + sheetIndex + "]:");
+        $.each(sheet.cssRules || sheet.rules, function(ruleIndex, rule) {
+            console.log("rule[" + ruleIndex + "]: " + rule.cssText);
+        });
+    });
+}
+
+function GetCurrentDocument() {
+    return window.document;
+}
+
+
+
+
+
+
+
 
 
 
