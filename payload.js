20f456f0a15bd0849e6726d18f65d47cb840f88c
kevinsoffera <kevinsoffera@gmail.com>
2023-01-30 17:50:20 -0500

    rename index to popup

--- payload.js
+++ payload.js
@@ -1,4 +1,8 @@
-function GetCurrentDocument() {
-    return window.document;
-}
+// Get colors
 
+$.each(document.styleSheets, function(sheetIndex, sheet) {
+    console.log("Looking at styleSheet[" + sheetIndex + "]:");
+    $.each(sheet.cssRules || sheet.rules, function(ruleIndex, rule) {
+        console.log("rule[" + ruleIndex + "]: " + rule.cssText);
+    });
+});
--- popup.js
+++ popup.js
@@ -31,30 +31,9 @@ function renderColors() {
     list.innerHTML = listItems
 }
 
-function scrape() {
-    $.each(document.styleSheets, function(sheetIndex, sheet) {
-        console.log("Looking at styleSheet[" + sheetIndex + "]:");
-        $.each(sheet.cssRules || sheet.rules, function(ruleIndex, rule) {
-            console.log("rule[" + ruleIndex + "]: " + rule.cssText);
-        });
-    });
-}
-
-
-
-
-
 
 
 
 /* 
 Add functionality to sort colors
 */
-
-// $(function () {
-
-//     $("button").click(function () {
-//         $("h1").hide();
-//     });
-
-// });
