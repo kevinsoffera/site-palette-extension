163a48fe848cf28d7d6883f63b4d80f433f264a0
kevinsoffera <kevinsoffera@gmail.com>
2023-02-21 21:57:45 -0500

    mouseover swatch to display color as text

--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 let myList = document.getElementById("list")
 
-// Inject content script on page load
+// Inject content script on popup load
 window.addEventListener('load', function (evt) {
 	browser.extension.getBackgroundPage().browser.tabs.executeScript(null, {
 		file: 'payload.js'
@@ -11,8 +11,10 @@ window.addEventListener('load', function (evt) {
 // Receive colors and render on popup
 browser.runtime.onMessage.addListener(function (message) {
     console.log(message)
-	// colors = message;
     renderColors(message)
+    console.log("rendered")
+    addTooltip()
+    console.log("tooltips")
 });
 
 
@@ -22,15 +24,32 @@ function renderColors(siteColors) {
     let listItems = ""
     for (let i = 0; i < colors.length; i++) {
         listItems += `
-            <div class="flex-item tooltiptext" style="background:${colors[i]};tooltip:${colors[i]}">
-                ${colors[i]}
+            <div class="swatch" style="background-color:${colors[i]}">
+                <span class="color-text">
+                    ${colors[i]}
+                </span>
             </div>
         `
     }
     document.getElementById("list").innerHTML = listItems
+    myList = document.getElementById("list")
+    // console.log(myList)
 }
 
 
-/* 
-Add functionality to sort colors
-*/
+function addTooltip() {
+
+    // selecting the elements for which we want to add a tooltip
+    var target = document.getElementsByClassName("swatch");
+    var colorText = document.getElementsByClassName("color-text");
+    console.log(colorText)
+    
+    
+    for (let x = 0; x < target.length; x++) {
+        // change display to 'block' on mouseover
+        target[x].addEventListener('mouseover', () => {colorText[x].style.display = 'block';}, false);
+        
+        // change display to 'none' on mouseleave
+        target[x].addEventListener('mouseleave', () => {colorText[x].style.display = 'none';}, false);
+    }
+}
